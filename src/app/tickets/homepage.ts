758ae518a94565b4652bb9df7df3bc018b4501f5
Jocelyne O <jojedace@my.centennialcollege.ca>
2022-12-13 22:54:22 -0500

    homepage button 2

--- src/app/tickets/homepage.ts
+++ src/app/tickets/homepage.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {Tickets} from "./models/tickets.model";
-import { TicketRepository } from "src/app/tickets/models/tickets.repository";
 import { Router } from "@angular/router";
 
 @Component({
